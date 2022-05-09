a0ad205430d625bc4edd5ac30a3594f63daa7a88
ToriRays <98709974+ToriRays@users.noreply.github.com>
2022-05-09 22:06:42 +0300

    Add files via upload

---
+++ palindrome.test.js
@@ -0,0 +1,10 @@
+const isPalindrome = require('./palindrome')
+ 
+test('it detects palindromes', () => {
+  expect(isPalindrome('palindrome')).toBe(false)
+  expect(isPalindrome('')).toBe(true)
+  expect(isPalindrome('a')).toBe(true)
+  expect(isPalindrome('gg')).toBe(true)
+  expect(isPalindrome('pop')).toBe(true)
+  expect(isPalindrome('1212')).toBe(false)
+})
